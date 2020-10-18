7be40f5b3eb3f312dc7b798ba8c2bc75db819ed1
repl.it user <replituser@example.com>
2020-10-18 16:38:49 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -96,13 +96,14 @@ int is_valid(Node* n){
   for(i=0;i<9;i++){
     for(j=0;j<9;j++){
       if(true == comp_linea_columna(i,j,n) && true == comp_cuadrante(i,j,n)){
-        cont++;
+        cont = 1;
       }else{
-        cont--;
+        cont = 0;
+        break;
       }
     }
   }
-  if(cont==81){
+  if(cont==1){
     return 1;
   }else{
     return 0;
